a525291fa953d7fce50a438b66fe47f60db8bc57
user <rimaa@danon.tzafonet.org.il>
2017-10-05 14:09:11 +0300

    hj

--- app/src/main/java/aghacom/rimaa/rimaashoppinglist/LogInActivity.java
+++ app/src/main/java/aghacom/rimaa/rimaashoppinglist/LogInActivity.java
@@ -1,5 +1,6 @@
 package aghacom.rimaa.rimaashoppinglist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,7 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class LogInActivity extends AppCompatActivity implements View.OnClickListener
+public class LogInActivity extends AppCompatActivity
 {
     private EditText etEmail;
     private EditText etPassw;
@@ -17,24 +18,30 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
         etEmail= (EditText) findViewById(R.id.etEmail);
         etPassw= (EditText) findViewById(R.id.etPassw);
         btnIn= (Button) findViewById(R.id.btnIn);
-        btnUp= (Button) findViewById(R.id.btnUp);
+        btnIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                    Intent i=new Intent(getBaseContext(),MainListActivity.class);
+                    startActivity(i);
+
+
+            }
+        });
+                btnUp= (Button) findViewById(R.id.btnUp);
         btnForget= (Button) findViewById(R.id.btnForget);
-    }
-
-    @Override
-    public void onClick(View view)
-    {
-        if (view==btnIn)
-        {
-
-        }
 
 
     }
-}
+
+
+
+    }
+
